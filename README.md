9b5178849273daa149170ad4490ce525b86a685a
Batu Sinanoglu <batusinanoglu@Batus-MacBook-Air.local>
2024-04-19 04:25:03 +0400

    finalized programs, adjusted dataset

--- README.md
+++ README.md
@@ -1,47 +1,26 @@
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=14782416&assignment_repo_type=AssignmentRepo)
-# FINAL PROJECT
+This catalyst project is aimed at determining whether there is a correlation between HDI rankings of a country, and mortality rate by injury.
 
-This repository is for all of the programs and files needed for your final project.
+The references for the datasets can be found in my catalyst slideshow: https://docs.google.com/presentation/d/1uAqKx7OXdyFciFhDk8tmUrkLMWAfAkCh6aS_cqR9Jxg/edit?usp=sharing
 
-Include any program files or data files that are needed for your visualizations when submitting to this assignment.
+=======================
 
-If you are pulling data from an API, please put the data into a file in this repository.
+> mortality_stats2.csv
 
-THANK YOU FOR A GREAT SEMESTER! :)
-
----
-
-## SPECIAL INSTRUCTIONS FOR API USERS
-
-Add a file named `config.py` and store your API keys in variables within this file. `config.py` has already been added to the `.gitignore` file.
-
-Add an `import config` to the top of your programs in order to use the API key. This will prevent the API key from being published to the web. You can then use the API key in your file by accessing the variable inside the config module, for example `config.my_api_key`.
+Conveniently named, houses the statistics of the mortality statistics of countries and territories recognized by the UN. The dates used are the last year that statistics were recorded for each country, which might lead to some inaccuracies in the way the data is then analyzed. Most of the data is ignored as the only important parts for my investigation include the 'Age_all' field, as I'm not measuring the differences in ages of the casualties.
 
 
-When using the requests library, you might need to `pip install requests`. If your computer complains about permissions, try installing for the user using `python -m pip install -user requests` on Windows or `pip install --user requests` on Mac.
 
----
+> hdi_rankings.csv
 
-### Tips
-- Start the project early!
-    - The earlier you start, the more time you have to get help if you run into any 
-    problems.
-- Test your code. 
-    - Find good stopping points to test out bits of code and
-    functions even when your program is only partially finished. 
-    - Try out different inputs and see if you expect the outputs that you get.
-- Ask for help on Twist. 
-    - Remember that we are traveling together on this 
-    learning journey and you don't have to struggle alone!
-    - It is way more important that you get your questions answered than it is
-    for you to have a perfectly working program
-- Try some [rubber duck debugging](https://rubberduckdebugging.com/).
-    - Find a rubber duck, or a pet rock, a stuffed animal, a figurine, a patient dog.
-    - Explain your program, line by line, to the victim of your choice.
-    - Sometimes this helps you think through your code and how it works, and 
-    can help you find bugs and errors in your code.
-- DO NOT COPY/PASTE CODE unless otherwise instructed to.
-    - The purpose of these exercises is for you to practice your programming skills.
-    - You will only harm yourself if you do not make the effort to understand the
-    programming concepts we are covering.
-    
+Also conveniently named, this CSV file includes all of the latest HDI rankings for nations across the world. Does not include all of the nations / territories used in the mortality statistics CSV.
+
+
+
+> data_analysis.py
+
+This is my work in analyzing the correlation between the two datasets. In this program, I create three graphs with trend lines in order to find the correlations, as well as preparing the datasets for these graphs.
+
+
+> data_analysis.html
+
+File created by Bokeh to visualize my graphs in HTML.
--- data_analysis.py
+++ data_analysis.py
@@ -21,7 +21,7 @@ f.close()
 # Creates three dictionaries with keys of country name, value of mortality via injury per 100,000 population
 # Three different dictionaries for All, Male, Female statistics
 
-f2 = open('mortality_stats.csv')
+f2 = open('mortality_stats2.csv')
 mortalitystats = csv.reader(f2)
 header2 = next(mortalitystats)
 
@@ -30,15 +30,16 @@ mortalitystatsDictFemale = {}
 mortalitystatsDictAll = {}
 
 for line in mortalitystats:
-    if line[5] == "All":
-        try: mortalitystatsDictAll[line[3]] = line[11]
-        except: continue
-    elif line[5] == "Male":
-        try: mortalitystatsDictMale[line[3]] = line[11]
-        except: continue
-    elif line[5] == "Female":
-        try: mortalitystatsDictFemale[line[3]] = line[11]
-        except: continue
+    if line[6] == "Age_all":
+        if line[5] == "All":
+            try: mortalitystatsDictAll[line[3]] = line[11]
+            except: continue
+        elif line[5] == "Male":
+            try: mortalitystatsDictMale[line[3]] = line[11]
+            except: continue
+        elif line[5] == "Female":
+            try: mortalitystatsDictFemale[line[3]] = line[11]
+            except: continue
 
 f2.close()
 
@@ -47,11 +48,25 @@ f2.close()
 
 hdiOrdered = []
 mortalityAllOrdered = []
+tempkey = ""
+
+translatorCSV = {"Brunei Darussalam": "Brunei", "Syrian Arab Republic": "Syria", "Cabo Verde": "NA", 
+"Czechia": "Czech Republic", "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
+"China, Hong Kong SAR": "China", "Republic of Moldova": "Moldova", "Russian Federation": "Russia", "T?rkiye": "Turkey",
+"Venezuela (Bolivarian Republic of)": "Venezuela", "Iran (Islamic Republic of)": "Iran", "Republic of Korea": "South Korea",
+"United States of America": "United States", "Guadeloupe": "NA", "French Guiana": "NA", "Martinique": "NA",
+"Mayotte": "NA", "R?union": "NA", "Puerto Rico": "NA"}
 
 for key in mortalitystatsDictAll:
+    if key in translatorCSV:
+        if translatorCSV[key] == "NA":
+            continue
+        tempkey = translatorCSV[key]
+    else: tempkey = key
+
     try: mortalityAllOrdered.append(float(mortalitystatsDictAll[key]))
     except: continue
-    hdiOrdered.append(float(hdirankingsDict[key]))
+    hdiOrdered.append(float(hdirankingsDict[tempkey]))
 
 # Graphs of the different metrics
 
@@ -59,7 +74,7 @@ fig = figure(title="Death by injury rate per 100 000 population vs HDI ranking o
 
 fig.xaxis.axis_label = "HDI"
 fig.yaxis.axis_label = "Death by injury rate per 100 000 population"
-fig.y_range = Range1d(0, 3000)
+fig.y_range = Range1d(0, 300)
 fig.x_range = Range1d(0, 1)
 fig.circle(hdiOrdered, mortalityAllOrdered, size=3, color = "red")
 
@@ -81,4 +96,83 @@ x = np.linspace(0, 2, 1000)
 
 fig.line(x, c + m * x)
 
-show(fig)
+mortalityMaleOrdered = []
+hdiOrdered = []
+
+for key in mortalitystatsDictMale:
+    if key in translatorCSV:
+        if translatorCSV[key] == "NA":
+            continue
+        tempkey = translatorCSV[key]
+    else: tempkey = key
+
+    try: mortalityMaleOrdered.append(float(mortalitystatsDictMale[key]))
+    except: continue
+    hdiOrdered.append(float(hdirankingsDict[tempkey]))
+
+fig2 = figure(title="Death by injury rate for per 100 000 male population vs HDI ranking of a country")
+
+fig2.xaxis.axis_label = "HDI"
+fig2.yaxis.axis_label = "Death by injury rate per 100 000 male population"
+fig2.y_range = Range1d(0, 300)
+fig2.x_range = Range1d(0, 1)
+fig2.circle(hdiOrdered, mortalityMaleOrdered, size=3, color = "purple")
+
+mean_x = np.mean(hdiOrdered)
+mean_y1 = np.mean(mortalityMaleOrdered)
+num = 0
+denom = 0
+
+for i in range(len(hdiOrdered)):
+    num += (hdiOrdered[i] - mean_x) * (mortalityMaleOrdered[i] - mean_y1)
+    denom += (hdiOrdered[i] - mean_x) ** 2
+
+m2 = num / denom
+c = mean_y1 - (m2 * mean_x)
+
+x = np.linspace(0, 2, 1000)
+
+fig2.line(x, c + m2 * x)
+
+mortalityFemaleOrdered = []
+hdiOrdered = []
+
+for key in mortalitystatsDictFemale:
+    if key in translatorCSV:
+        if translatorCSV[key] == "NA":
+            continue
+        tempkey = translatorCSV[key]
+    else: tempkey = key
+
+    try: mortalityFemaleOrdered.append(float(mortalitystatsDictFemale[key]))
+    except: continue
+    hdiOrdered.append(float(hdirankingsDict[tempkey]))
+
+fig3 = figure(title="Death by injury rate for per 100 000 female population vs HDI ranking of a country")
+
+fig3.xaxis.axis_label = "HDI"
+fig3.yaxis.axis_label = "Death by injury rate per 100 000 female population"
+fig3.y_range = Range1d(0, 300)
+fig3.x_range = Range1d(0, 1)
+fig3.circle(hdiOrdered, mortalityFemaleOrdered, size=3, color = "green")
+
+mean_x = np.mean(hdiOrdered)
+mean_y1 = np.mean(mortalityFemaleOrdered)
+num = 0
+denom = 0
+
+for i in range(len(hdiOrdered)):
+    num += (hdiOrdered[i] - mean_x) * (mortalityFemaleOrdered[i] - mean_y1)
+    denom += (hdiOrdered[i] - mean_x) ** 2
+
+m3 = num / denom
+c = mean_y1 - (m3 * mean_x)
+
+x = np.linspace(0, 2, 1000)
+
+fig3.line(x, c + m3 * x)
+
+p = gridplot([[fig, fig2, fig3]])
+show(p)
+
+print(f"Whole population gradient: {str(m)}\nMale population gradient: {str(m2)}\nFemale population gradient: {str(m3)}")
